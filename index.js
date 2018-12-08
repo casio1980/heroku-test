e9c5ae05271e7eec7533f06978e47603a8366298
Aleksandr Andreev <aleksandr_andreev@epam.com>
2018-12-08 23:00:01 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = process.env.port;
+
+http.createServer((request, response) => {
+    console.log(request.url);
+    response.end('Hello Node.js Server!');
+}).listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+    console.log(`Listening on port ${port}`);
+});
